e2e01a4197369f6d4f0c07da619dcea4eb921e8f
lapiks <dhenin.julien@gmail.com>
2022-08-07 18:49:11 +0200

    math: math types import from glam

--- crates/verdi-math/src/lib.rs
+++ crates/verdi-math/src/lib.rs
@@ -1,1 +1,13 @@
 pub use glam::*;
+
+#[allow(missing_docs)]
+pub mod prelude {
+    #[doc(hidden)]
+    pub use crate::{
+        Vec2, Vec3, Vec4,
+        IVec2, IVec3, IVec4,
+        Mat2, Mat3, Mat4,
+        Quat,
+        //...   
+    };
+}
