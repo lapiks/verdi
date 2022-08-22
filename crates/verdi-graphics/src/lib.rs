56982bd05218b6254cc91bcffd70572fcea1a38c
lapiks <dhenin.julien@gmail.com>
2022-08-22 17:50:54 +0200

    verdi-graphics: gouraud shading

--- crates/verdi-graphics/src/lib.rs
+++ crates/verdi-graphics/src/lib.rs
@@ -33,14 +33,17 @@ pub fn run() {
                                           &teapot::INDICES).unwrap();
 
     let vertex_shader_src =  r#"
-        #version 140
+        #version 150
 
         in vec3 position;
         in vec3 normal;
         
+        out vec3 v_normal;
+        
         uniform mat4 matrix;
         
         void main() {
+            v_normal = transpose(inverse(mat3(matrix))) * normal;
             gl_Position = matrix * vec4(position, 1.0);
         }
     "#;
@@ -49,10 +52,15 @@ pub fn run() {
     let fragment_shader_src = r#"
         #version 140
 
+        in vec3 v_normal;
         out vec4 color;
-
+        uniform vec3 u_light;
+        
         void main() {
-            color = vec4(1.0, 0.0, 0.0, 1.0);
+            float brightness = dot(normalize(v_normal), normalize(u_light));
+            vec3 dark_color = vec3(0.6, 0.0, 0.0);
+            vec3 regular_color = vec3(1.0, 0.0, 0.0);
+            color = vec4(mix(dark_color, regular_color, brightness), 1.0);
         }
     "#;
     //let fragment_shader = shader::Shader::new(fragment_shader_src.to_string());
@@ -67,13 +75,16 @@ pub fn run() {
         }
     };
 
-    
+    // uniforms
     let matrix = [
         [0.01, 0.0, 0.0, 0.0],
         [0.0, 0.01, 0.0, 0.0],
         [0.0, 0.0, 0.01, 0.0],
         [0.0, 0.0, 0.0, 1.0f32]
     ];
+
+    // the direction of the light
+    let light = [-1.0, 0.4, 0.9f32];
 
     event_loop.run(move |ev, _, control_flow| {
 
@@ -84,7 +95,7 @@ pub fn run() {
             (&positions, &normals), 
             &indices, 
             &program, 
-            &uniform! { matrix: matrix },
+            &uniform! { matrix: matrix, u_light: light },
             &Default::default()
         ).unwrap();
 
