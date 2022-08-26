c8a02d7786824ac06965a720c11cdbac202fe158
lapiks <dhenin.julien@gmail.com>
2022-08-26 20:11:10 +0200

    lua scripting with rlua

--- crates/verdi-app/src/app.rs
+++ crates/verdi-app/src/app.rs
@@ -1,4 +1,6 @@
 use glium::{glutin, Surface, uniform};
+use rlua::{Function, Lua, MetaMethod, Result, UserData, UserDataMethods, Variadic};
+use std::{path::Path, fs::File, error::Error, io::Read};
 
 use verdi_graphics::prelude::*;
 
@@ -7,13 +9,27 @@ pub struct App {
 }
 
 impl App {
-    pub fn run() {
+    pub fn run() -> Result<()> {
         let event_loop = glutin::event_loop::EventLoop::new();
         let wb = glutin::window::WindowBuilder::new();
         let cb = glutin::ContextBuilder::new();
         let display = glium::Display::new(wb, cb, &event_loop).unwrap();
         
         let mut gpu = GraphicsChip::new(display).unwrap();
+
+        // lua scripting
+        let script_code = App::load_script("./game_example/game.lua");
+
+        let lua = Lua::new();
+
+        lua.context(|lua_ctx| {
+            let globals = lua_ctx.globals();
+
+            lua_ctx.load(&script_code).eval::<()>()?;
+            lua_ctx.load("update()").eval::<()>()?;
+
+            Ok(())
+        })?;
 
         event_loop.run(move |ev, _, control_flow| {
             gpu.render();
@@ -33,5 +49,13 @@ impl App {
                 _ => (),
             }
         });
+    }
+
+    fn load_script<P: AsRef<Path>>(path: P) -> String {
+        // todo : gestion d'erreur
+        let mut f = File::open(path).unwrap();
+        let mut content: String = String::new();
+        f.read_to_string(&mut content).unwrap();
+        content
     }
 }
