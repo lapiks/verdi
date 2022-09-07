fe61c0963d68ec5a23239222ebadf868904177fc
lapiks <dhenin.julien@gmail.com>
2022-09-07 16:01:15 +0200

    Lua scripting continuation

--- crates/verdi-graphics/src/bind_graphics_chip.rs
+++ crates/verdi-graphics/src/bind_graphics_chip.rs
@@ -7,19 +7,39 @@ use crate::{prelude::GraphicsChip, graphics_chip::PrimitiveType};
 pub struct BindGraphicsChip;
 
 impl BindGraphicsChip {
+    fn beginObject(gpu: &Mutex<GraphicsChip>, primitive_type: String) {
+        let mut enum_val = PrimitiveType::triangles;
+        if primitive_type == "triangles" { enum_val = PrimitiveType::triangles; }
+        else if primitive_type == "points" { enum_val = PrimitiveType::points; }
+        else if primitive_type == "lines" { enum_val = PrimitiveType::lines; }
+
+        gpu.lock().unwrap().begin(enum_val);
+    }
+
+    fn endObject(gpu: &Mutex<GraphicsChip>) {
+        gpu.lock().unwrap().end();
+    }
+
     pub fn bind(lua: &Lua, gpu: &'static Mutex<GraphicsChip>) -> Result<()> {
         lua.context(|lua_ctx| {
             let globals = lua_ctx.globals();
     
             // create graphics module table
-            let map_table = lua_ctx.create_table()?;
-                    
+            let module_table = lua_ctx.create_table()?;
+            
             // add functions
-            let func = lua_ctx.create_function_mut(|_, ()| Ok(gpu.lock().unwrap().end()))?;
-            map_table.set("endObject", func)?;
+            {
+                let func = lua_ctx.create_function_mut(|_, primitive_type: String| Ok(BindGraphicsChip::beginObject(gpu, primitive_type)))?;
+                module_table.set("beginObject", func)?;
+            }
+            {
+                let func = lua_ctx.create_function_mut(|_, ()| Ok(BindGraphicsChip::endObject(gpu)))?;
+                module_table.set("endObject", func)?;
+            }
+            
     
             // add table to globals
-            globals.set("graphics", map_table)?;
+            globals.set("graphics", module_table)?;
     
             Ok(())
         })?;
--- game_example/game.lua
+++ game_example/game.lua
@@ -7,5 +7,6 @@ function verdi.update()
 end
 
 function verdi.draw()
+    graphics.beginObject("triangles")
     graphics.endObject()
 end
