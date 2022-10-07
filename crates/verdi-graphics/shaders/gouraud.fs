79271a2e5b51716c9c95c4a0330f0404555a0cd6
lapiks <dhenin.julien@gmail.com>
2022-10-07 14:52:07 +0200

    fix texture rendering

--- crates/verdi-graphics/shaders/gouraud.fs
+++ crates/verdi-graphics/shaders/gouraud.fs
@@ -8,6 +8,6 @@ out vec4 color;
 uniform sampler2D u_texture;
 
 void main() {
-    color = texture(u_texture, v_uv);
+    color = texture(u_texture, vec2(v_uv.x, 1.0 - v_uv.y)); // y inversion ?
     //color = v_color;
 }
