07ec1580807de09b409af43f5260169b5ea40049
lapiks <dhenin.julien@gmail.com>
2023-03-05 09:39:38 +0100

    Audio continuation

--- crates/verdi-audio/src/audio.rs
+++ crates/verdi-audio/src/audio.rs
@@ -1,6 +1,6 @@
 use std::{rc::Rc, cell::RefCell, path::Path};
 
-use rodio::{OutputStream, OutputStreamHandle, Source};
+use rodio::{OutputStream, OutputStreamHandle, Sink};
 use slotmap::SlotMap;
 
 use crate::audio_clip::{AudioClipHandle, AudioClipId, AudioClip};
@@ -22,22 +22,40 @@ impl AudioHandle {
     }
 
     pub fn play_clip(&self, clip: AudioClipHandle) {
-        self.inner.borrow_mut().play_clip(clip)
+        self.inner.borrow_mut().play_clip(clip);
     }
 }
 
 pub struct Audio {
     clips: SlotMap<AudioClipId, AudioClip>,
-    stream_handle: OutputStreamHandle,
+    stream: Option<OutputStream>,
+    stream_handle: Option<OutputStreamHandle>,
+    sinks: Vec<Sink>,
 }
 
 impl Audio {
     pub fn new() -> Self {
-        let (_, stream_handle) = OutputStream::try_default().unwrap();
-        Self {
-            clips: SlotMap::default(),
-            stream_handle,
+        if let Ok((stream, stream_handle)) = OutputStream::try_default() {
+            Self {
+                clips: SlotMap::default(),
+                stream: Some(stream),
+                stream_handle: Some(stream_handle),
+                sinks: Vec::default(),
+            }
         }
+        else {
+            println!("No audio device found.");
+            Self {
+                clips: SlotMap::default(),
+                stream: None,
+                stream_handle: None,
+                sinks: Vec::default(),
+            }
+        }
+    }
+
+    pub fn add_sink(&mut self, sink: Sink) {
+        self.sinks.push(sink);
     }
 
     pub fn new_clip<P: AsRef<Path>>(&mut self, path: P) -> AudioClipHandle {
@@ -48,10 +66,28 @@ impl Audio {
         AudioClipHandle::new(id)
     }
 
-    pub fn play_clip(&self, clip: AudioClipHandle) {
-        if let Some(clip) = self.clips.get(clip.id) {
-            let source = clip.get_source();
-            self.stream_handle.play_raw(source.convert_samples());
+    pub fn play_clip(&mut self, clip_handle: AudioClipHandle) {
+        if let Some(clip) = self.clips.get(clip_handle.id) {
+            if let Some(sink) = self.stream_handle
+                .as_ref()
+                .and_then(|stream_handle, | match Sink::try_new(stream_handle) {
+                    Ok(sink) => {
+                        let source = clip.get_source();
+                        sink.append(source);
+                        Some(sink)
+                    }
+                    Err(err) => {
+                        println!("Error playing sound: {err:?}");
+                        None
+                    }
+                })
+                {
+                    self.sinks.push(sink);
+                }
+
+        }
+        else {
+            println!("Error playing sound: Audio source not found."); 
         }
     } 
 }
